fe698c65dce7f79820f1b7bf10d1065079d37707
Anith Kumar <anithkumartp016631@gmail.com>
2020-10-16 17:59:27 +0800

    init

--- Main.js
+++ Main.js
@@ -7,9 +7,9 @@ hamburgerMenu.addEventListener('click', function(){
  
 const options = { 
   bottom: '64px', // default: '32px'
-  right: 'unset', // default: '32px'
-  left: '32px', // default: 'unset'
-  time: '0.5s', // default: '0.3s'
+  right: '32px', // default: '32px'
+  left: 'unset', // default: 'unset'
+  time: '0.3s', // default: '0.3s'
   mixColor: '#fff', // default: '#fff'
   backgroundColor: '#fff',  // default: '#fff'
   buttonColorDark: '#100f2c',  // default: '#100f2c'
